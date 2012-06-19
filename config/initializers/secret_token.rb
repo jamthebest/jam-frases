e2474f49d913d4d4e56565d9eddb323a7986f721
Javier Midence <javier.midence@unitec.edu>
2012-06-19 13:54:17 -0600

    Iniciando Frases

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frases::Application.config.secret_token = 'dc83063e447b68e1c103a28a6422f4515e535b04a9553df97fd03f7f0ef399660ce63b69e6de0a7faf41308164b9beecf2f2168ad17dd4161a4c904ba45be220'

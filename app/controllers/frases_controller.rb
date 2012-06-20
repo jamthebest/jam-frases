35f630e9744ce1f85744960abe65ddeccc21a744
Javier Midence <javmidence@yahoo.es>
2012-06-20 17:52:47 -0600

    Modificando Controller Frases

--- app/controllers/frases_controller.rb
+++ app/controllers/frases_controller.rb
@@ -1,22 +1,63 @@
 class FrasesController < ApplicationController
   def index
+    @frases = Frase.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @frase }
+    end
   end
 
   def create
+    @frase = Frase.new(params[:id])
+    respond_to do |format|
+      if @frase.save
+        format.html { redirect_to @frase, flash[:notice] = 'Frase Agregada!' }
+        format.json { render json: @frase, status: :created, location: @frase }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @frase.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def new
+    @frase = Frase.new
+    respond_to do |format|
+      format.html
+      format.json { render json: @frase }
+    end
   end
 
   def update
+    @frase = Frase.find(params[:id])
+    respond_to do |format|
+      if @frase.update_attributes(params[:frase])
+        format.html { redirect_to @frase, flash[:notice] = 'Frase Actualizada!' }
+        format.json { render json: @frase, status: :created, location: @frase }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @frase.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
+    @frase = Frase.find(params[:id])
   end
 
   def show
+    @frase = Frase.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @frase }
+    end
   end
 
   def destroy
+    Frase.find(params[:id]).try(:delete)
+    respond_to do |format|
+      format.html { redirect_to frases_path }
+      format.json { head :no_content }
+    end
   end
 end
